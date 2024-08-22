0a669e17c2e7d52a86db2605008221ac64ef5836
Nicolas Paone <nicolasarielpaone@gmail.com>
2024-08-21 23:38:18 -0300

    Reemplazo de las operaciones por metodos

--- src/ar/edu/unlam/pb2/interfaz/Calculadora.java
+++ src/ar/edu/unlam/pb2/interfaz/Calculadora.java
@@ -11,44 +11,64 @@ public class Calculadora {
         Integer a; 
         Integer b; 
         Integer opcion; 
-        Integer resultado; 
+        Integer resultado = 0; 
         
         do{
         	
-        System.out.println("Ingrese su operación /n 1 para multiplicar /n 2 para restar /n 3 para sumar /n 4 para dividir"); 
+        	mostrarPorPantalla("Ingrese su operación /n 1 para multiplicar /n 2 para restar /n 3 para sumar /n 4 para dividir"); 
         
-        opcion = teclado.nextInt(); 
+        	opcion = teclado.nextInt(); 
         
         }while(opcion < 1 && opcion > 4); 
         
         
-        System.out.println("Ingrese el primer numero"); 
+        mostrarPorPantalla("Ingrese el primer numero"); 
         a = teclado.nextInt(); 
         
-        System.out.println("Ingrese el segundo numero"); 
+        mostrarPorPantalla("Ingrese el segundo numero"); 
         b = teclado.nextInt(); 
         
         switch (opcion) {
-        
         case 1:
-        	resultado = a * b;
+        	multiplicacion(a, b);
         	break;
-        	
         case 2:
-        	resultado = a - b;
+        	resta(a, b);
         	break;
-        	
         case 3:
-        	resultado = a + b; 
+        	suma(a, b);
         	break;
-        	
         default:
-    		resultado = a / b; 
+        	division(a, b);
     		break;
         }
 		
-        System.out.println("El resultado es " + resultado.toString()); 
+        mostrarPorPantalla("El resultado es " + resultado.toString()); 
         
         teclado.close();
 	} 
+	
+	private static void mostrarPorPantalla(String mensaje) {
+		System.out.println(mensaje);
+	}
+	
+	private static Integer suma(Integer a, Integer b) {
+		Integer resultado = a + b;
+		return resultado;
+	}
+	
+	private static Integer resta(Integer a, Integer b) {
+		Integer resultado = a - b;
+		return resultado;
+	}
+	
+	private static Integer multiplicacion(Integer a, Integer b) {
+		Integer resultado = a * b;
+		return resultado;
+	}
+	
+	private static Integer division(Integer a, Integer b) {
+		Integer resultado = a / b;
+		return resultado;
+	}
 }
